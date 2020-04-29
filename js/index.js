51262bfc25cd0227cf3908e5f6602a6092ea8dbf
myari211 <syukrihsb148@gmail.com>
2020-04-29 15:14:35 +0700

    :white_check_mark: Add design and stylish

--- js/index.js
+++ js/index.js
@@ -1,11 +1,81 @@
+var stylish = `<div class="article">
+    <img src="assets/img/design1.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/design2.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/design3.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/design4.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/design5.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/design6.jpg" alt="" />
+</div>`
+
+var design = `<div class="article">
+    <img src="assets/img/web1.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/web2.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/web3.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/web4.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/web5.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/web6.jpg" alt="" />
+</div>`
+
+var allServices = `<div class="article">
+    <img src="assets/img/1.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/2.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/3.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/4.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/5.jpg" alt="" />
+</div>
+<div class="article">
+    <img src="assets/img/6.jpg" alt="" />
+</div>`
+
 var contentHeadItems = document.querySelectorAll(".content-head span")
-console.log("contentHeadItems", contentHeadItems)
 
 for (let i = 0; i < contentHeadItems.length; i++) {
     const element = contentHeadItems[i]
-    console.log("element", element)
     element.addEventListener("click", function () {
         document.querySelector(".content-head .active").classList.remove("active")
         element.classList.toggle("active")
+
+        switch (i) {
+            case 0:
+                document.querySelector(".article-wrapper").innerHTML = allServices
+                break
+            case 1:
+                document.querySelector(".article-wrapper").innerHTML = design
+                break
+            case 2:
+                document.querySelector(".article-wrapper").innerHTML = stylish
+                break
+            default:
+                document.querySelector(".article-wrapper").innerHTML = allServices
+                break
+        }
     })
 }
